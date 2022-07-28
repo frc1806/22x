e0128a365927601397b4477adf7eab8ae733caa0
NatFletch <nef111605@yahoo.com>
2022-07-27 19:38:28 -0500

    subsystem template and some more thingies
    cleanup
    
    jaming
    
    ignore build

---
+++ src/main/java/frc/robot/subsystem/Subsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystem;
+
+public interface Subsystem {
+    public void writeToLog();
+
+    public abstract void stop();
+
+    public abstract void zeroSensors();
+
+    public abstract void setDebug(boolean _debug);
+
+    public abstract void retractAll();
+
+    public abstract void setupDriverTab();
+}
